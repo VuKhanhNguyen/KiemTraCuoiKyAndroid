a19b04d698a1e973bfeeb8c318dbc58efdac3d43
khanhnguvcl <nguyenvukhanh392@gmail.com>
2024-12-21 09:53:11 +0700

    khanh

--- Nhm13/build.gradle.kts
+++ Nhm13/build.gradle.kts
@@ -1,4 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
+//    classpath("com.android.tools.build:gradle:8.5.2") // Thay đổi phiên bản AGP ở đây
 }
